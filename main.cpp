2cb852eb2959c2fa65560e1bd006f4de1c10caca
Tax <taxkall2007@gmail.com>
2025-02-19 00:58:51 +0200

    Added priority feature

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ using namespace std;
 struct Task{
 	string description;
 	bool completed;
+	int priority; 
+	/*
+		1 - low priority
+		2 - medium priority
+		3 - high priority
+		4 - critical priority
+	*/
 };
 
 
@@ -19,7 +26,7 @@ void saveTasks(vector<Task> &tasks){
 	}
 	
 	for (auto& task : tasks){
-		fout<<task.description<<";"<<task.completed<<"\n";
+		fout<<task.description<<";"<<task.completed<<";"<<task.priority<<"\n";
 	}
 	
 	fout.close();
@@ -35,11 +42,18 @@ void loadTasks(vector<Task> &tasks){
 	
 	string desc;
 	bool completed;
-	while(getline(fin, desc, ';') && fin>>completed){
+	int prior;
+	while(fin.peek() != EOF){
+		getline(fin, desc, ';');
+		fin >> completed;
 		fin.ignore();
+		fin>> prior;
+		fin.ignore();
+
 		Task t;
 		t.description = desc;
 		t.completed = completed;
+		t.priority = prior;
 		tasks.push_back(t);
 	}
 	
@@ -63,7 +77,8 @@ int main(){
 		cout<<"3. Mark a task as completed.\n";
 		cout<<"4. Delete a task.\n";
 		cout<<"5. Update a task's description.\n";
-		cout<<"6. Exit.\n";
+		cout<<"6. Update a task's priority.\n";
+		cout<<"7. Exit.\n";
 		cout<<"Enter action's number: ";
 		cin>>choice;
 		
@@ -72,10 +87,22 @@ int main(){
 			cout<<"\nEnter task description: ";
 			string desc;
 			getline(cin, desc);
+			
+			cout<<"Enter priority (1=Low, 2=Medium, 3=High, 4=Critical): ";
+			int priority;
+			cin>>priority;
+
+			while (priority < 1 || priority > 4){
+				cout<<"Invalid priority please re-enter: ";
+				cin>>priority;
+			}
+
 			Task t;
 			t.description = desc;
 			t.completed = false;
+			t.priority = priority;
 			tasks.push_back(t);
+
 			saveTasks(tasks);
 			cout<<"Task added! \n";			
 		}else if (choice == 2){		// VIEW TASKS
@@ -84,7 +111,18 @@ int main(){
 			}else{
 				cout<<"\nTo-Do list:\n";
 				for(int i=0; i<tasks.size(); i++){
-					cout<< i + 1 <<". ["<<(tasks[i].completed ? "✔" : " ")<<"] "<<tasks[i].description<<endl; // boolCase ? true : false
+					string priority;
+					switch (tasks[i].priority){
+						case 1:
+							priority = "Low Priority "; break;
+						case 2:
+							priority = "Medium Priority "; break;
+						case 3:
+							priority = "High Priority "; break;
+						case 4:
+							priority = "Critical Priority "; break;
+					}
+					cout<< i + 1 <<". ["<<(tasks[i].completed ? "DONE" : " ")<<"] "<<tasks[i].description<<"	// "<<priority<<"\n"; // boolCase ? true : false
 				}
 			}			
 		}else if (choice == 3){		// MARK COMPLETED TASK
@@ -104,7 +142,7 @@ int main(){
 			}
 		}else if (choice == 4){		// DELETE TASK
 			if (tasks.empty()){
-				cout<<"\nNo tasks available.\n";
+				cout<<"\nThere are no available tasks.\n";
 			}else{
 				cout<<"\nEnter the task's number you wish to delete: ";
 				int taskNumber;
@@ -119,9 +157,9 @@ int main(){
 			}
 		}else if (choice == 5){
 			if (tasks.size() == 0){
-				cout<<"There are no available tasks.\n";
+				cout<<"\nThere are no available tasks.\n";
 			}else{
-				cout<<"Enter the task's number which you wish to change the description: ";
+				cout<<"\nEnter the task's number which you wish to change the description: ";
 				int taskNumber;
 				cin>>taskNumber;
 				if (taskNumber < 1 || taskNumber > tasks.size()){
@@ -137,13 +175,35 @@ int main(){
 				}
 			}
 		}else if (choice == 6){
+			if (tasks.size() == 0){
+				cout<<"\nThere are no available tasks.\n";
+			}else{
+				cout<<"\nPlease enter the task's whose priority you wish to change: ";
+				int taskNumber;
+				cin>>taskNumber;
+				if (taskNumber < 1 || taskNumber > tasks.size()){
+					cout<<"Invalid task.\n";
+				}else{
+					cout<<"Please enter the new priority (1=Low, 2=Medium, 3=High, 4=Critical): ";
+					int priority;
+					cin>>priority;
+					while (priority < 1 || priority > 4){
+						cout<<"Invalid priority please re-enter: ";
+						cin>>priority;
+					}
+					tasks[taskNumber-1].priority = priority;
+					saveTasks(tasks);
+					cout<<"Successfuly updated task's priority.\n";
+				}
+			}
+		}else if (choice == 7){
 			cout<<"\nExiting... \n";
 		}else{
 			cout<<"\nInvalid input please try again. \n";
 		}
 		
 		
-	} while (choice != 6);
+	} while (choice != 7);
 	
 	
 	return 0;
